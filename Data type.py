b01c9c0bb05f82edebbbfe4974b1318aa940d87b
siddhipatil75 <siddhipatil489@gmail.com>
2025-07-18 02:47:45 -0700

    Add files via upload

---
+++ Data type.py
@@ -0,0 +1,55 @@
+
+num1 = int(input("Enter num1"))          
+num2 = int(input("Enter num2"))
+name = "Python"     
+is_active = True     
+
+
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+modulo = num1 % 3
+
+
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
+print("Modulo:", modulo)
+
+
+is_equal = num1 == num2
+is_not_equal = num1 != num2
+is_greater = num1 > num2
+is_less = num1 < num2
+
+print("Equal?", is_equal)
+print("Not Equal?", is_not_equal)
+print("Greater?", is_greater)
+print("Less?", is_less)
+
+
+logic_and = (num1 > 5) and (num2 < 5)  
+logic_or = (num1 < 5) or (num2 < 5)
+logic_not = not is_active
+
+print("Logical AND:", logic_and)
+print("Logical OR:", logic_or)
+print("Logical NOT:", logic_not)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
